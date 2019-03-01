7460af91266f3d356c8f0909a6054afd90b61407
Robbie <txt3rob@gmail.com>
2019-03-01 12:35:04 +0000

    tweet

--- bot.py
+++ bot.py
@@ -1,5 +1,21 @@
 import os
 import random
+import tweepy
+
+consumer_key = (os.environ['consumer_key'])
+consumer_secret = (os.environ['consumer_secret'])
+access_token = (os.environ['access_token'])
+access_token_secret = (os.environ['access_token_secret'])
+  
+# authentication of consumer key and secret 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret) 
+  
+# authentication of access token and secret 
+auth.set_access_token(access_token, access_token_secret) 
+api = tweepy.API(auth) 
+  
+
+
 
 def rline (filename):
 	with open(filename) as f:
@@ -16,5 +32,6 @@ C = rline("countries.txt")
 noun = rline("nouns.txt")
 animal = rline("animals.txt")
 
-msg = ""+twit+" is accused of creating "+malware+" & working as a proxy of "+C+" APT group known as "+noun+" "+animal+" "
+msg = "."+twit+" is accused of creating "+malware+" & working as a proxy of "+C+" APT group known as "+noun+" "+animal+" "
 print (msg)
+api.update_status(status=msg)
